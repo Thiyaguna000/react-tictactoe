2416d4a5c6e575f1e73d47d939435109c9c75d2b
Thiyagu <thiyaguna000@gmail.com>
2022-08-09 11:48:51 +0100

    added flat array

--- src/components/Speech.js
+++ src/components/Speech.js
@@ -1,25 +1,60 @@
-import React,{ useState } from 'react';
-import './Counter.css';
+import React, { useEffect, useState } from "react";
+import "./Counter.css";
 
 const Speech = () => {
-    const [inputText,setInputText] = useState("");
-    const msg = new SpeechSynthesisUtterance();
+  const [inputText, setInputText] = useState("");
+  const msg = new SpeechSynthesisUtterance();
 
-    const inputHandler = (e) =>{
-        setInputText(e.target.value);
-    }
+  const inputHandler = (e) => {
+    setInputText(e.target.value);
+  };
 
-    const speechHandler = (msg) => {
-        msg.text = inputText
-        window.speechSynthesis.speak(msg)
-      }
+  const speechHandler = (msg) => {
+    msg.text = inputText;
+    window.speechSynthesis.speak(msg);
+  };
+
+  const groups = [
+    {
+      title: "Group A",
+      products: [
+        { sku: 123, price: 1.29 },
+        { sku: 234, price: 1.69 },
+      ],
+    },
+    {
+      title: "Group B",
+      products: [
+        { sku: 345, price: 1.49 },
+        { sku: 456, price: 4.58 },
+      ],
+    },
+  ];
+
+  const products = () => {
+    // let productList = [];
+   const productList = groups.map(element => {
+      return element.products
+    });
+    //let flattened = productList.flat(1);
+    let flattened = productList.reduce((a,b)=> a.concat(b));
+    flattened.sort((a,b) => b.price-a.price);
+    console.log(flattened);
+  } 
+
+  useEffect(()=>{
+    products();
+  },[])
 
   return (
     <div>
-        <input type="text" value={inputText} onChange={inputHandler} />
-        <button onClick={() => speechHandler(msg)}>Speak!</button>
-    </div>
-  )
-}
+      <input type="text" value={inputText} onChange={inputHandler} />
+      <button onClick={() => speechHandler(msg)}>Speak!</button>
+      <div>
 
-export default Speech
+      </div>
+    </div>
+  );
+};
+
+export default Speech;
